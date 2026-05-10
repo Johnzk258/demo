5b880bce878057b68c0a4036dea6234a1f0449df
Johnzk258 <76684635+Johnzk258@users.noreply.github.com>
2026-05-10 11:23:13 +0800

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './defaults.css';
-import Shop from './Shop';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Shop />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
